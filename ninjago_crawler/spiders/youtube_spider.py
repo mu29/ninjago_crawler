ee1257b1e1d0ae2bb068b376c9c88d85800641c0
InJung Chung <mu29gl@gmail.com>
2016-03-21 19:08:02 +0900

    비디오 정보 크롤링

--- ninjago_crawler/spiders/youtube_spider.py
+++ ninjago_crawler/spiders/youtube_spider.py
@@ -1,4 +1,8 @@
+from ninjago_crawler.items import VideoItem
 import scrapy
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 class YoutubeSpider(scrapy.Spider):
     name = "youtube"
@@ -8,6 +12,13 @@ class YoutubeSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        data_list = response.xpath('//tr[@class="pl-video yt-uix-tile "]')
+
+        for data in data_list:
+            video = VideoItem()
+            video['title'] = data.xpath('.//td[@class="pl-video-title"]/a/text()')[0].extract().replace('\n', '')
+            video['video_id'] = data.xpath('.//@data-video-id')[0].extract()
+            video['thumbnail_url'] = 'http://i.ytimg.com/vi/%s/default.jpg' % video['video_id']
+            video['minute'] = data.xpath('.//td[@class="pl-video-time"]/div/div/span/text()')[0].extract()
+            #http://i.ytimg.com/vi/9WHv9Nf9WAA/default.jpg
+            yield video
